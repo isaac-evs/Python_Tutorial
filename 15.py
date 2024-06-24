4d7945fa9e1b3d8483b9decf985d815b4064f07d
isaac-evs <isaac.vazquez@iteso.mx>
2024-06-24 17:57:54 -0600

    Added 15.py

---
+++ 15.py
@@ -0,0 +1,17 @@
+# Complex if statements
+
+money_avaible = 100
+hungry = True
+bored = True
+
+#if money_avaible >= 80 and hungry == True or bored == True:
+#    print("Eat something fancy")
+
+#########################
+
+if money_avaible >= 80:
+    print("I have enough money")
+    if hungry:
+        print("And Im hungry")
+        if bored:
+            print("Eat something fancy")
