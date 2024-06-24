32edfb31931b78d5dad85522c381dd333a825eef
isaac-evs <isaac.vazquez@iteso.mx>
2024-06-24 17:10:58 -0600

    added 14.py

---
+++ 14.py
@@ -0,0 +1,11 @@
+
+money_avaible = 100
+
+if money_avaible >= 80:
+    print("Eat something fancy")
+elif money_avaible > 45:
+    print("Eat somehing nice")
+elif money_avaible > 15:
+    print("Eat something okay")
+else:
+    print("Eat something cheap")
