9370ebbe0920af612d9d2406d10f134edc9f9eec
isaac-evs <isaac.vazquez@iteso.mx>
2024-06-24 14:29:02 -0600

    added 13.py

---
+++ 13.py
@@ -0,0 +1,12 @@
+my_dict = {1: "one", 2: "two", 3: "three"}
+
+print(1 in my_dict.keys())
+print("four" in my_dict.values())
+
+###########################
+
+print(bool(123))  #true
+print(bool(0))    #false
+print(bool(" "))  #true
+print(bool(""))   #false
+print(bool(None)) #false
