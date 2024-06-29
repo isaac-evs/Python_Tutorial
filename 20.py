f7578171e0ec917bcb2b63fd9fe8f39670464472
isaac-evs <122956526+isaac-evs@users.noreply.github.com>
2024-06-28 23:39:09 -0600

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,6 @@
+# Parameters
+
+def greeter_function(person = "Person", greet = "Hello", weekday = "Monday"):
+    print(f"{greet}, {person} today is {weekday}")
+
+greeter_function("Bob", weekday = "Tuesday")
