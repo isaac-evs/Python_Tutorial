2b6d0b74fce245e596b9049b083356547d4a6fc0
isaac-evs <122956526+isaac-evs@users.noreply.github.com>
2024-06-29 20:08:50 -0600

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,12 @@
+# Lambda (single line functions)
+
+simple_calculator = lambda a,b: a + b
+
+print(simple_calculator(10,20))
+
+#####################################
+
+
+my_function = lambda n: "Hello" if n > 5 else "Bye"
+
+print(my_function(10))
