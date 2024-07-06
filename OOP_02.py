e408e4ff45461692e25ba7be5c59f808c980cfa8
isaac-evs <isaac.vazquez@iteso.mx>
2024-07-06 12:10:59 -0600

    modified OOP2

--- OOP_02.py
+++ OOP_02.py
@@ -1,17 +1,29 @@
 # Dunder methods
 
-# Classes 
-
 class Monster:
 
     #attributes
-    health = 90
-    energy = 40
-
     def __init__(self, health, energy):
         self.health = health
         self.energy = energy
 
+    #Other dunder methods
+
+    def __len__(self):
+        return self.health
+
+    def __abs__(self):
+        return self.energy
+    
+    def __call__(self):
+        return "The monster was called"
+    
+    def __add__(self, other):
+        return self.health + other
+    
+    def __str__(self):
+        return f"A monster with health = {self.health}, and energy = {self.energy}"
+    
     #methods
     def attack(self, amount):
         print("The monster has attacked")
@@ -24,8 +36,15 @@ class Monster:
         print(f"moving at {speed} km/h")
 
 
-monster1 = Monster()
-monster2 = Monster()
+monster1 = Monster(10,20)
+print(len(monster1))
+print(abs(monster1))
 
-print(monster1.health)
-print(monster2.health)
+print(monster1.__dict__)
+print(vars(monster1))
+
+print(monster1())
+
+print(monster1 + 10)
+
+print(monster1)
