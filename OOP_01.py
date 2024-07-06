66d23c2297fd6c5236dabe2a57e428ae09a12a4f
isaac-evs <isaac.vazquez@iteso.mx>
2024-07-05 22:35:40 -0600

    modified 00P01

--- OOP_01.py
+++ OOP_01.py
@@ -7,13 +7,13 @@ class Monster:
     energy = 40
 
     #methods
-    def attack(monster, amount):
+    def attack(self, amount):
         print("The monster has attacked")
         print(f"{amount} demage was dealt")
         monster.energy -=10
         print(monster.energy)
     
-    def move(monster, speed):
+    def move(self, speed):
         print("The monster has moved")
         print(f"moving at {speed} km/h")
 
