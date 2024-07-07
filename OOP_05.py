85841eaabd313144205ff14ce80763d03d083388
isaac-evs <isaac.vazquez@iteso.mx>
2024-07-06 21:01:18 -0600

    added OOP 05

---
+++ OOP_05.py
@@ -0,0 +1,34 @@
+# Scope and Classes (Excercise)
+
+class Monster:
+
+    def __init__(self, health, energy):
+        self.health = health
+        self.energy = energy
+    
+    def get_damage(self, ammount):
+        monster.health -= ammount
+
+class Hero:
+
+    def __init__(self, damage, monster):
+        self.damage = damage
+        self.monster = monster
+
+    def attack(self, ammount):
+        monster.get_damage(ammount)
+
+
+monster = Monster(health = 100, energy = 50)
+
+
+hero = Hero(damage = 20, monster = monster)
+
+
+print(monster.health)
+
+
+hero.attack(20)
+
+
+print(monster.health)
