cd53c1e6b07165a9f78f1130bb5707f11fb83b87
isaac-evs <isaac.vazquez@iteso.mx>
2024-07-09 14:27:16 -0600

    added 34.py

---
+++ 34.py
@@ -0,0 +1,9 @@
+# External modules
+
+import matplotlib.pyplot as plt
+
+plt.plot ([1,2,3,4], [1,4,9, 16], "ro")
+plt.axis((0, 7, 0, 21))
+plt.ylabel("some numbers")
+plt.xlabel("other numbers")
+plt.show()
