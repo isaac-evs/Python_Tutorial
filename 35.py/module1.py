54f112cfd903ab4eacc21631b393f97dcc62dcb0
isaac-evs <isaac.vazquez@iteso.mx>
2024-07-09 18:12:36 -0600

    added 35.py (two files)

--- 35.py/module1.py
+++ 35.py/module1.py
@@ -2,3 +2,5 @@
 def sum_calculator(*args):
     result = sum(args)
     print(result)
+
+print(__name__)
--- 35.py/module2.py
+++ 35.py/module2.py
@@ -1,3 +1,8 @@
 from module1 import sum_calculator
 
 sum_calculator(1,3,4,5,6,7,8)
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("the main file")
